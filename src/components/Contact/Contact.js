e0121c26545ea742b649169baad488ce8a738dba
Julien <goetghebuer.julien@gmail.com>
2020-01-16 16:45:55 +0100

    form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,15 +4,17 @@ import FormInput from "./FormInput/FormInput";
 import Title from "../Title/Title";
 
 const Contact = props => (
-  <form>
-    <Title>Need an Events ?</Title>
-    <FormInput>Name :</FormInput>
-    <FormInput>Last Name :</FormInput>
-    <FormInput>Email :</FormInput>
-    <label>Description</label>
-    <textarea cols="30" rows="10"></textarea>
-    <input type="submit" name="SEND" />
-  </form>
+  <div className={styles.Contact}>
+    <Title className={styles.Title}>Need an Events ?</Title>
+    <form className={styles.Form}>
+      <FormInput className={styles.Name}>Name :</FormInput>
+      <FormInput className={styles.LastName}>Last Name :</FormInput>
+      <FormInput className={styles.Email}>Email :</FormInput>
+      <label>Description</label>
+      <textarea className={styles.TextArea} cols="30" rows="10"></textarea>
+      <input type="submit" name="SEND" />
+    </form>
+  </div>
 );
 
 export default Contact;
